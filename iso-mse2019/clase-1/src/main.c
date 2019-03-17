091a4af5474da8a6f444f3ac3b8e7077e61c4024
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-03-17 07:20:21 -0300

    Se agrega variable para manejar el estado dee las tareas que pueden ser RUNNING o BLOCKED, tambien sen se agrega la funcion delay y una funcion que se ejecuta en el get next context que se llamaa update_task que se erncarga de decrementar los contadores de los delays. Se agrega una tarea idle para poder volver en el caso que las dos tareas esten en idle-Se cambia toda la logica del get next context para saltar a la misma tarea en el caso que la otra este bloqueada. Si las dos estan ocupadas salta a la idle. La idle solo tiene un __WFI

--- iso-mse2019/clase-1/src/main.c
+++ iso-mse2019/clase-1/src/main.c
@@ -4,69 +4,114 @@
 #include "string.h"
 
 #define STACK_SIZE_B 512
+typedef enum {
+	RUNNING, BLOCKED
+} task_state_t;
 
-typedef void * (* task_type)(void *);
+typedef enum {
+	MAIN_STACK = 0, TASK1, TASK2, IDLE_TASK
+} task_index_t;
 
-uint32_t stack_1 [STACK_SIZE_B/4];
-uint32_t stack_2 [STACK_SIZE_B/4];
+typedef void * (*task_type)(void *);
 
-uint32_t stack_1_pointer;
-uint32_t stack_2_pointer;
+uint32_t stack_1[STACK_SIZE_B / 4];
+uint32_t stack_2[STACK_SIZE_B / 4];
+uint32_t stack_idle[STACK_SIZE_B / 4];
 
-uint32_t current_task;
+uint32_t stack_1_pointer, stack_2_pointer, stack_idle_pointer;
 
+uint32_t task1_delay, task2_delay;
+task_state_t task1_state, task2_state, idle_state;
 
-void * task1 (void * arg){
-	uint32_t tick_count = 250;
-	while(1){
-		if(tick_count == 0){
-			tick_count = 250;
-			gpioToggle(LED1);
-		}
-		else
-			tick_count--;
-		__WFI();
+task_index_t current_task = MAIN_STACK;
+
+void task_delay(uint32_t delay) {
+	if (delay == 0) {
+		return;
 	}
-	return NULL;
+	switch (current_task) {
+	case 1:
+		task1_state = BLOCKED;
+		task1_delay = delay;
+		break;
+	case 2:
+		task2_state = BLOCKED;
+		task2_delay = delay;
+		break;
+	default:
+		while (1) {
+			__WFI();
+		}
+	}
+	//Sera esta la funcion para esperar hasta que termine. La idea seria
+	//llamar directamente al scheduler.
+	__WFI();
 }
 
-void * task2 (void * arg){
-	uint32_t tick_count = 500;
-	while(1){
-		if(tick_count == 0){
-			tick_count = 500;
-			gpioToggle(LED2);
+void update_tasks(void) {
+	if (task1_state == BLOCKED) {
+		if (--task1_delay == 0) {
+			task1_state = RUNNING;
 		}
-		else
-			tick_count--;
-		__WFI();
 	}
-	return NULL;
+	if (task2_state == BLOCKED) {
+		if (--task2_delay == 0) {
+			task2_state = RUNNING;
+		}
+	}
 }
 
-
-uint32_t get_next_context (uint32_t current_sp){
+uint32_t get_next_context(uint32_t current_sp) {
 
 	uint32_t next_sp;
-
-	switch(current_task){
-	case 0:
+	update_tasks();
+	switch (current_task) {
+	case MAIN_STACK:
 		/*Vere que hacer con el contexto inicial*/
 		next_sp = stack_1_pointer;
 		current_task = 1;
 		break;
-	case 1:
+	case TASK1:
 		stack_1_pointer = current_sp;
-		next_sp =stack_2_pointer ;
-		current_task = 2;
+		if (task2_state == RUNNING) {
+			next_sp = stack_2_pointer;
+			current_task = TASK2;
+		} else if (task1_state == RUNNING) {
+			next_sp = stack_1_pointer;
+			current_task = TASK1;
+		} else {
+			next_sp = stack_idle_pointer;
+			current_task = IDLE_TASK;
+		}
 		break;
-	case 2:
+	case TASK2:
 		stack_2_pointer = current_sp;
-		next_sp =stack_1_pointer;
-		current_task = 1;
+		if(task1_state == RUNNING){
+			next_sp = stack_1_pointer;
+			current_task = TASK1;
+		}else if (task2_state == RUNNING) {
+			next_sp = stack_2_pointer;
+			current_task = TASK2;
+		}else {
+			next_sp = stack_idle_pointer;
+			current_task = IDLE_TASK;
+		}
+		break;
+	case IDLE_TASK:
+		stack_idle_pointer = current_sp;
+		if (task1_state == RUNNING) {
+			next_sp = stack_1_pointer;
+			current_task = TASK1;
+		} else if (task2_state == RUNNING) {
+			next_sp = stack_2_pointer;
+			current_task = TASK2;
+		} else {
+			next_sp = stack_idle_pointer;
+			current_task = IDLE_TASK;
+		}
 		break;
 	default:
-		while(1){
+		while (1) {
 			__WFI();
 		}
 		break;
@@ -74,39 +119,64 @@ uint32_t get_next_context (uint32_t current_sp){
 	return next_sp;
 }
 
-void task_return_hook (void * ret_val){
-	while (1){
+void task_return_hook(void * ret_val) {
+	while (1) {
 		__WFI();
 	}
 }
 
-void init_stack (	uint32_t stack[],
-					uint32_t stack_byte_size,
-					uint32_t *sp,
-					task_type entry_point,
-					void * arg){
-
-	bzero(stack,stack_byte_size);
-
-	stack[stack_byte_size/4-1] = 1 << 24;				/*XPSR.T = 1*/
-	stack[stack_byte_size/4-2] = (uint32_t) entry_point; 			/*PC*/
-	stack[stack_byte_size/4-3] = (uint32_t) task_return_hook;		/*LR*/
-	stack[stack_byte_size/4-8] = (uint32_t) arg; 					/*R0*/
-	stack[stack_byte_size/4-9] = 0xFFFFFFF9;			/*LR IRQ*/
-
-	*sp =(uint32_t) &(stack[stack_byte_size/4-17]); //Corri el stack point 8 lugares hacia donde crece la pila
+//Tarea IDLE para irme en caso que tenga algun incoveniente
+void * idle_task(void * arg) {
+	while (1) {
+		__WFI();
+	}
 }
 
+void * task1(void * arg) {
+	uint32_t tick_count = 2000;
+	while (1) {
+		gpioToggle(LED1);
+		task_delay(tick_count);
+	}
+	return NULL;
+}
 
+void * task2(void * arg) {
+	uint32_t tick_count = 4000;
+	while (1) {
+		gpioToggle(LED2);
+		task_delay(tick_count);
+	}
+	return NULL;
+}
 
+void init_stack(uint32_t stack[], uint32_t stack_byte_size, uint32_t *sp,
+		task_type entry_point, void * arg) {
 
+	bzero(stack, stack_byte_size);
 
+	stack[stack_byte_size / 4 - 1] = 1 << 24; /*XPSR.T = 1*/
+	stack[stack_byte_size / 4 - 2] = (uint32_t) entry_point; /*PC*/
+	stack[stack_byte_size / 4 - 3] = (uint32_t) task_return_hook; /*LR*/
+	stack[stack_byte_size / 4 - 8] = (uint32_t) arg; /*R0*/
+	stack[stack_byte_size / 4 - 9] = 0xFFFFFFF9; /*LR IRQ*/
 
-int main (void){
+	*sp = (uint32_t) &(stack[stack_byte_size / 4 - 17]); //Corri el stack point 8 lugares hacia donde crece la pila
+}
+
+int main(void) {
 
 	// Inicializar y configurar la plataforma
-	init_stack(stack_1,STACK_SIZE_B,&stack_1_pointer,task1, (void *)0x11223344);
-	init_stack(stack_2,STACK_SIZE_B,&stack_2_pointer,task2, (void *)0x11223344);
+	init_stack(stack_1, STACK_SIZE_B, &stack_1_pointer, task1,
+			(void *) 0x11223344);
+	init_stack(stack_2, STACK_SIZE_B, &stack_2_pointer, task2,
+			(void *) 0x11223344);
+	init_stack(stack_idle, STACK_SIZE_B, &stack_idle_pointer, idle_task,
+			(void *) 0x11223344);
+
+	task1_state = RUNNING;
+	task2_state = RUNNING;
+	idle_state = RUNNING;
 
 	//Board_Init();
 	//SystemCoreClockUpdate();
@@ -115,7 +185,7 @@ int main (void){
 	SystemCoreClockUpdate();
 	SysTick_Config(SystemCoreClock / 1000);
 
-	while(1){
+	while (1) {
 		__WFI();
 	}
 }
