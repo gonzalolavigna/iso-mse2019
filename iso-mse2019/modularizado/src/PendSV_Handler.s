8799160057777ce07b0dc8f1fcc0190f05dceb7b
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-03 10:49:59 -0300

    Se agrega a empezar el trabajo modularizado

---
+++ iso-mse2019/modularizado/src/PendSV_Handler.s
@@ -0,0 +1,16 @@
+	.syntax unified
+	.text
+	.global PendSV_Handler
+	.extern get_next_context
+	.thumb_func
+
+	PendSV_Handler:
+		push {r4-r11,lr} 		/**Siempre guardar el link register**/
+		mrs r0,msp
+		bl get_next_context
+		msr msp,r0 				/** msp = r0 *//*Valor devuelto por get_next_context*/
+
+		pop {r4-r11,lr}
+
+	return:
+		bx lr
