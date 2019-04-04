8295570723cd5e84bd766706edbe8730bb078e9a
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-04 03:48:54 -0300

    Se agrega un envio por UART a la tarea 3 mientras se dejan los leds de los GPIO para probar las prioridades

--- iso-mse2019/colas/src/task3.c
+++ iso-mse2019/colas/src/task3.c
@@ -10,10 +10,15 @@ uint32_t task3_stack[TASK3_STACK_SIZE_BYTES/4];
 void* task3 (void* a){
 	uint32_t i;
 	gpioInit(GPIO2,GPIO_OUTPUT);
+	gpioWrite(GPIO2,OFF);
 	while(1){
+
+		uartWriteString(UART_USB,"TASK 3 TESTING TASK 3 TASK 3\r\n");
+		gpioWrite(GPIO2,ON);
 		os_task_delay(2000);
-		gpioToggle(LED3);
-		gpioToggle(GPIO2);
+		gpioWrite(GPIO2,OFF);
+		//gpioToggle(LED3);
+		//gpioToggle(GPIO2);
 	}
 }
 
