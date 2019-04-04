5e84c93869afdd8df038773f12f7de9e51432d7c
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-04 03:20:57 -0300

    Version probado con una cola de prioridad con tres tareas en la prioridad mas alta

---
+++ iso-mse2019/colas/inc/task3.h
@@ -0,0 +1,18 @@
+#ifndef __TASK3__
+#define __TASK3__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define TASK3_STACK_SIZE_BYTES 512
+
+void* task3 (void* a);
+void* hook3 (void* p);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
