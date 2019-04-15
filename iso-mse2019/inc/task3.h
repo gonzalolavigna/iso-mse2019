74684923e9640b5bb8c27c72a3e8f95fccc4e3ff
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-14 23:35:25 -0300

    Se agrega a la tarea que haga un extern del evento tecla_event para que lo vean las tareas que llamas a task3.h

--- iso-mse2019/inc/task3.h
+++ iso-mse2019/inc/task3.h
@@ -1,13 +1,22 @@
 #ifndef __TASK3__
 #define __TASK3__
 
+#include <stdint.h>
+#include "string.h"
+#include "os.h"
+#include "os_delay.h"
+#include "os_event.h"
+#include "sapi.h"
+
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #define TASK3_STACK_SIZE_BYTES 512
 
-extern uint32_t task3_stack[];
+extern 	uint32_t 						task3_stack[];
+extern 	os_event_handler_t 	tecla_event;
 
 void* task3 (void* a);
 void* hook3 (void* p);
