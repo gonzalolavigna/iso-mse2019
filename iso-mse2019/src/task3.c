d00424cf4533e9849e6828715e6c9bb9d84f80ae
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-14 23:36:36 -0300

    Esta tarea solo imprimia un mensaje constante cada 2 segundo ahora se hace que imprima cosas solamente cuando la destraban, en si esta sincronizada con el evento tecla_event

--- iso-mse2019/src/task3.c
+++ iso-mse2019/src/task3.c
@@ -2,25 +2,35 @@
 #include "string.h"
 #include "os.h"
 #include "os_delay.h"
+#include "os_event.h"
 #include "sapi.h"
 #include "task3.h"
 
 
-uint32_t task3_stack[TASK3_STACK_SIZE_BYTES/4];
+//Estos datos se comparten externamente
+uint32_t 						task3_stack[TASK3_STACK_SIZE_BYTES/4];
+os_event_handler_t 	tecla_event;
 
 void* task3 (void* a){
 	uint32_t i;
+	//Mensaje de bienvenida de la Tarea 3 para
+	uartWriteString(UART_USB,"TASK 3-EVENTOS\r\n");
 	gpioInit(GPIO2,GPIO_OUTPUT);
-	gpioWrite(GPIO2,OFF);
+	gpioWrite(GPIO2,ON);
 	while(1){
 
-		uartWriteString(UART_USB,"TASK 3 TESTING TASK 3 TASK 3-FPU CONTEXT\r\n");
-		gpioWrite(GPIO2,ON);
-		os_task_delay(2000);
-		gpioWrite(GPIO2,OFF);
-		//gpioToggle(LED3);
-		//gpioToggle(GPIO2);
+		if(os_event_wait(tecla_event) == TRUE){
+			//Este evento va a provenir de la Tarea 4 Hacemos togglear un GPIO para poder
+			//verlo con el analizador logico
+			gpioToggle(GPIO2);
+			uartWriteString(UART_USB,"EVENTO TECLA DETECTADO\r\n");
+			printf("PAPUCHO\r\n");
+			gpioToggle(GPIO2);
+		} else {
+			uartWriteString(UART_USB,"PROBLEMA ESPERANDO EVENTO\r\n");
+		}
 	}
+	return (void *)TRUE;
 }
 
 void* hook3 (void* p) {
