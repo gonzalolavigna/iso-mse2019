8e223fe13878989c0bd8383278872c631994f0ae
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-17 21:07:08 -0300

    Se agrega una implementacion que lee rapidamente el vector con el estado de las teclas, tal vez agregar un mutex una vez que el evento se libera, se imprime por la UART cuanto duro la tecla pulsa y que tecla

--- iso-mse2019/src/task3.c
+++ iso-mse2019/src/task3.c
@@ -5,30 +5,35 @@
 #include "os_event.h"
 #include "sapi.h"
 #include "task3.h"
+#include "task4.h"
 
 
 //Estos datos se comparten externamente
 uint32_t 						task3_stack[TASK3_STACK_SIZE_BYTES/4];
 os_event_handler_t 	tecla_event;
 
+//TODO: Este arreglo de 4 eventos de teclas
+debounce_data_t tecla_array_copy[4];
+
 void* task3 (void* a){
 	uint32_t i;
 	//Mensaje de bienvenida de la Tarea 3 para
-	uartWriteString(UART_USB,"TASK 3-EVENTOS\r\n");
+	uartWriteString(UART_USB,"TASK 3-EVENTOS-TECLAS-UART-WRITE\r\n");
 	gpioInit(GPIO2,GPIO_OUTPUT);
 	gpioWrite(GPIO2,ON);
 	while(1){
-
 		if(os_event_wait(tecla_event) == TRUE){
-			//Este evento va a provenir de la Tarea 4 Hacemos togglear un GPIO para poder
-			//verlo con el analizador logico
-			gpioToggle(GPIO2);
-			uartWriteString(UART_USB,"EVENTO TECLA DETECTADO\r\n");
-			printf("PAPUCHO\r\n");
-			gpioToggle(GPIO2);
+			copy_tecla_array(tecla_array_copy);
+			for(i=0;i<4;i++){
+				if(tecla_array_copy[i].tecla_liberada_event == TRUE){
+					printf("TECLA %d PRESIONADA POR:%d TICKS\r\n",i+1,tecla_array_copy[i].ticks_presionada);
+				}
+			}
+
 		} else {
 			uartWriteString(UART_USB,"PROBLEMA ESPERANDO EVENTO\r\n");
 		}
+		gpioToggle(GPIO2);
 	}
 	return (void *)TRUE;
 }
