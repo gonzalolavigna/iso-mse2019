7073d9d1c4996aeb7a732dbef22443a22becf0a0
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-17 21:11:15 -0300

    Se agrega la FSM para parsear las tecla con un delay y libera un semaforo para que la uart imprima, se genera una funcion para hqacer una copia rapida y tambien como siempre hay que hacer una funcion para inicializar en un valor valido las teclas

--- iso-mse2019/src/task4.c
+++ iso-mse2019/src/task4.c
@@ -9,19 +9,96 @@
 
 uint32_t task4_stack[TASK4_STACK_SIZE_BYTES/4];
 
+gpioMap_t teclas[] = {TEC1,TEC2,TEC3,TEC4};
+#define TECLAS_VALIDAS sizeof(teclas)/sizeof(gpioMap_t)
+
+debounce_data_t tecla_array[TECLAS_VALIDAS];
+
+void debounce_init(void){
+	uint32_t i;
+	for(i=0;i<TECLAS_VALIDAS;i++){
+		tecla_array[i].state = BUTTON_UP;
+		tecla_array[i].tecla = teclas[i];
+		tecla_array[i].tecla_liberada_event = FALSE;
+		tecla_array[i].tecla_presionada_event = FALSE;
+		tecla_array[i].ticks_presionada = 0;
+		tecla_array[i].primer_tick = 0;
+	}
+}
+
+
 
 void* task4 (void* a){
-	//Despues esto va a crecer para poder ver mas teclas lo hacemos solo por tiempo
+	//Inicializamos la FSM de las teclas
+	uint32_t i;
+	bool_t 	 hit = FALSE;
+	debounce_init();
+
 	while(1){
-		//Lo hacemos esperar 1s y le damos el give
-		os_task_delay(1000);
-		while(os_event_set(tecla_event)==FALSE);
-		//Lo hacemos esperar 2s y le damos el give
-		os_task_delay(2000);
-		while(os_event_set(tecla_event)==FALSE);
-		//Lo hacemos esperar 4s y le damos el give
-		os_task_delay(4000);
-		while(os_event_set(tecla_event)==FALSE);
+		//Hacemos una espera inicial de 10ms en la tarea
+		//Al principio no tenemos ningun evento que informar
+		//Este codigo detecta un evento de tecla que se levanta
+		hit = FALSE;
+		for(i=0;i<TECLAS_VALIDAS;i++){
+			switch(tecla_array[i].state){
+			case BUTTON_UP:
+					tecla_array[i].tecla_liberada_event = FALSE;
+					if(!gpioRead(tecla_array[i].tecla)){
+						tecla_array[i].state = BUTTON_FALLING;
+						tecla_array[i].ticks_presionada = 0;
+						tecla_array[i].primer_tick = 0;
+					}
+				break;
+			case BUTTON_FALLING:
+					if(!gpioRead(tecla_array[i].tecla)){
+						//Esto quiere decir que el tiempo del debouncer fue superado con exito
+						tecla_array[i].state = BUTTON_DOWN;
+						tecla_array[i].primer_tick = os_get_ticks_running();
+					}
+					else {
+						//Vuelvo a estar en BUTTON_UP
+						tecla_array[i].state = BUTTON_UP;
+					}
+				break;
+			case BUTTON_DOWN:
+				 if(gpioRead(tecla_array[i].tecla))
+					 tecla_array[i].state = BUTTON_RAISING;
+				break;
+			case BUTTON_RAISING:
+					if(gpioRead(tecla_array[i].tecla)){
+						tecla_array[i].state = BUTTON_UP;
+						tecla_array[i].ticks_presionada = os_get_ticks_running() - tecla_array[i].primer_tick;
+						tecla_array[i].tecla_liberada_event = TRUE;
+						//Si tenga un hit TRUE liberamos y seteamos el evento.
+						hit = TRUE;
+					}
+				break;
+			default:
+				break;
+			}
+		}
+		//SI alguna tecla se solto ponemos el evento que se solto
+		//una tecla
+		if(hit == TRUE) {
+			os_event_set(tecla_event);
+		}
+		//Esto lo ponemos en el delay con 10.
+		os_task_delay(10);
+	}
+}
+
+
+//Esto hace una copia rapida para hacer las cosas
+void copy_tecla_array(debounce_data_t * dst ){
+	uint32_t i;
+	//Esto hace una copia bastante rapida
+	for(i=0;i<TECLAS_VALIDAS;i++){
+		dst[i].primer_tick 						= tecla_array[i].primer_tick;
+		dst[i].state 			 						= tecla_array[i].state;
+		dst[i].tecla 			 						= tecla_array[i].tecla;
+		dst[i].tecla_liberada_event 	= tecla_array[i].tecla_liberada_event;
+		dst[i].tecla_presionada_event = tecla_array[i].tecla_presionada_event;
+		dst[i].ticks_presionada 			= tecla_array[i].ticks_presionada;
 	}
 }
 
@@ -31,3 +108,4 @@ void* hook4 (void* p){
 	}
 	return (void*)TRUE;
 }
+
