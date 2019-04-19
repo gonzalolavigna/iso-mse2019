00750cad3270aa906ef2af57adb0d839fd884641
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-19 20:59:22 -0300

    Esta tarea no cambio mucho solo se reubicaron cosas en el proceso de debug porque se rompia el sapi buffer con el printf en float se toco mucho esta funcion, actualmente quedamos con la misma aplicacion funcion y se cambia el nombre uart_buffer por uart_buffer_sapi para que sea mas claro

--- iso-mse2019/src/task6.c
+++ iso-mse2019/src/task6.c
@@ -7,24 +7,29 @@
 #include "sapi_circularBuffer.h"
 #include "task6.h"
 
-void uart_buffer_full_message(void *);
 
+//Definicion para el stack de la tarea 6
 uint32_t task6_stack[TASK6_STACK_SIZE_BYTES/4];
 
+//Definicion del buffer circular para poder recibir los datos desde la UART
 uint8_t  uart_buffer_array[256];
-circularBuffer_t uart_buffer;
+circularBuffer_t uart_buffer_sapi;
+
+//Funcion para printear cuando se llena el buffer circular para recibir por UART
+void uart_buffer_full_message(void *);
+
 
 /*Inicializamos el buffer circular de la SAPI*/
 void init_uart_circular_buffer(void){
-	circularBuffer_Init(&uart_buffer,uart_buffer_array,256,sizeof(uint8_t));
-	circularBufferFullBufferCallbackSet(&uart_buffer,uart_buffer_full_message);
+	circularBuffer_Init(&uart_buffer_sapi,uart_buffer_array,256,sizeof(uint8_t));
+	circularBufferFullBufferCallbackSet(&uart_buffer_sapi,uart_buffer_full_message);
 }
 
 void uart_buffer_full_message(void * no_usado){
 	//Para que no se mezclean los mensajes y ejecute sola
 	//estamos en un error asi que no importa mucho si bloque
 	os_enter_critical();
-	printf("UART CIRCULAR BUFFER FULL REVISAR THROUGHPUT");
+	printf("UART CIRCULAR BUFFER FULL REVISAR THROUGHPUT\r\n");
 	os_quit_critical();
 }
 
@@ -32,8 +37,9 @@ void uart_buffer_full_message(void * no_usado){
 void* task6	(void* a){
 	uint8_t temp_data;
 	while(1){
-		if(circularBufferRead(&uart_buffer,&temp_data) == CIRCULAR_BUFFER_EMPTY){
-			//Esta tarea la paramos por 100 ms para que otras hagan sus cosas
+		if(circularBufferRead(&uart_buffer_sapi,&temp_data) == CIRCULAR_BUFFER_EMPTY){
+			//Esta tarea la paramos por 100 ms para que otras hagan sus cosas/ solo se despierta
+			//para ver si hay algo en el buffer circular y lo reenvia por la UART.
 			os_task_delay(100);
 		}
 		else {
