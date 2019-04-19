53eb817cd7e09f83843b6082893fd77cf12e1029
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-19 00:54:50 -0300

    Se implementa la API para los mutex, tambien se modifica los eventos para que en el caso de hacer un wait se carge en el event-task_waiting la tarea que hizo el wait esto se debe a que cualuier de las dos tareas que pueden trabar un mutex pueden entrar en cualquier orden. Los mutex arrancan el evento en set dle que hacer uso, ya que el primer lock pasa de larga y solo traba el segundo

--- iso-mse2019/src/os_event.c
+++ iso-mse2019/src/os_event.c
@@ -5,7 +5,9 @@
 #include "task_stack.h"
 
 event_t 	event_list[MAX_EVENT_COUNT];
+mutex_t 	mutex_list[MAX_MUTEX_COUNT];
 uint32_t 	event_count = 0;
+uint32_t 	mutex_count = 0;
 
 //Inicializo la estructura de la lista de evento
 void os_event_init_array(void){
@@ -63,12 +65,13 @@ bool_t os_event_wait	(os_event_handler_t event){
 		ev->state = EVENT_WAIT;
 		break;
 	case EVENT_SET:
-		if((running_task_index > task_count) || (ev->task_waiting != running_task_index)){
+		if((running_task_index > task_count)){
 			//Esta es una condición imposible ya que una vez que una tarea que hizo el primer wait
 			//no puede cambiar. Es restrictivo
 			os_error_hook();
 		}
 		ev->state = EVENT_WAIT;
+		ev->task_waiting = running_task_index;
 		//Habilitamos una sección donde el Systick nos puede cambiar el estado de las tareas
 		disable_sys_tick_irq();
 		task_list[running_task_index].event_waiting = TRUE;
@@ -130,3 +133,86 @@ bool_t os_event_set	(os_event_handler_t event){
 	//cosas -> Tal vez se podria hacer algo con las prioridades para ver si hacer o no un cambio
 	//de contexto
 }
+
+
+
+void os_mutex_init_array(void){
+	int i;
+	for(i=0;i<MAX_MUTEX_COUNT;i++){
+		mutex_list[i].mutex_state= MUTEX_UNLOCK;
+		//Las tareas que apunta las ponemos en el maximo posible + 1
+		mutex_list[i].event = NULL;
+	}
+}
+
+os_mutex_handler_t os_mutex_init(void){
+	os_event_handler_t ev_handler;
+	os_mutex_handler_t mutex_handler;
+
+	if(mutex_count >= MAX_MUTEX_COUNT){
+		return NULL;
+	}
+	//Obivamente un mutex utiliza un semaforo
+	//Si no pudo crear el evento directamente devuelvo NULL
+	if((ev_handler=os_event_init())==NULL){
+		return NULL;
+	}
+	//El evento del mutex queda asignado
+	mutex_list[mutex_count].event = ev_handler;
+	//Todos los mutex arrancan en unlock
+	mutex_list[mutex_count].mutex_state = MUTEX_UNLOCK;
+	//Aranca la FSM de un lado distinto ya que el evento esta en set por defecto
+	//Hace un lock me da directamente al estado de wait en la proxima vez que pase
+	((event_t*)mutex_list[mutex_count].event)->state = EVENT_SET;
+	mutex_handler = &mutex_list[mutex_count];
+	mutex_count++;
+	return mutex_handler;
+
+}
+
+
+bool_t	os_mutex_lock 	(os_mutex_handler_t mutex){
+	mutex_t* local_mutex= (mutex_t*)mutex;
+	switch(local_mutex->mutex_state){
+	case MUTEX_LOCK:
+		//Cuando el mutex esta lock hay que esperar que alguien lo libere por lo tanto
+		//ponemos a la tarea que lo llama a dormir
+		if(os_event_wait(local_mutex->event) == FALSE){
+			uartWriteString(UART_USB,"OS:ERROR EN MUTEX LOCK");
+		}
+		break;
+	case MUTEX_UNLOCK:
+		local_mutex->mutex_state = MUTEX_LOCK;
+		break;
+	default:
+		os_error_hook();
+		break;
+	}
+	return TRUE;
+}
+
+
+bool_t os_mutex_unlock (os_mutex_handler_t mutex){
+	mutex_t* local_mutex= (mutex_t*)mutex;
+	switch(local_mutex->mutex_state){
+	case MUTEX_LOCK:
+		//Como esta en lock mandamos el evento a la tarea
+		if(os_event_set(local_mutex->event)== FALSE){
+			uartWriteString(UART_USB,"OS:ERROR EN MUTEX UNLOCK");
+		}
+		local_mutex->mutex_state = MUTEX_UNLOCK;
+		break;
+	case MUTEX_UNLOCK:
+		//Hacer un unlock cuando esta unlock no importa
+		//ya que esta seria un caso en el que se lockea algo y desppues se unlockea
+		//sin ningun tarea pedir acceso a la sección crítica
+		break;
+	default:
+		os_error_hook();
+		break;
+	}
+	return TRUE;
+}
+
+
+
