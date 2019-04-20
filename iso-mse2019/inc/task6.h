3fc4e0985b173b296c6b9dd92fe9578b06056f23
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-19 21:01:31 -0300

    Se acomoa para que quede el entorno igual a los otros .h se aumenta el tamaño del stack de esta tarea porque se creia que podia ser un stack overflow al final no era eso pero se deja un tamaño de 2048 para probar con distintos tamaños de stack, se cambia el nombre de uart_buffer por uart_buffer_sapi y se hace un extern del arreglo donde van a quedar guardados los datos almacenados de la UART, creo que estos no son necesarios para el extern

--- iso-mse2019/inc/task6.h
+++ iso-mse2019/inc/task6.h
@@ -6,18 +6,26 @@
 #include "os.h"
 #include "os_delay.h"
 #include "os_event.h"
+#include "sapi_circularBuffer.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define TASK6_STACK_SIZE_BYTES 512
+#define TASK6_STACK_SIZE_BYTES 2048
 
 extern	uint32_t 					task6_stack[];
-extern 	circularBuffer_t 	uart_buffer;
+//Hacemos un extern para que otros puedan usar el buffer circular de la SAPI
+extern  uint8_t 	 				uart_buffer_array[];
+extern 	circularBuffer_t 	uart_buffer_sapi;
 
 void init_uart_circular_buffer(void);
 void* task6										(void* a);
 void* hook6										(void* p);
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
