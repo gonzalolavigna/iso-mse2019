bfbfbf32acb2b8a8d4ef3ad0322deb89de9eb200
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-24 18:42:44 -0300

    Para ver la impresion por punto flotante usando la version en C de util.c se hace algo en la task1 ahora se comenta pero se deja en comentado por si hay que volver a usarla

--- iso-mse2019/src/task1.c
+++ iso-mse2019/src/task1.c
@@ -4,15 +4,21 @@
 #include "os_delay.h"
 #include "sapi.h"
 #include "task1.h"
+#include "utils.h"
 
 
 uint32_t task1_stack[TASK1_STACK_SIZE_BYTES/4];
 
 void* task1 (void* a){
 	uint32_t i;
+	uint8_t print_string[16];
 	gpioInit(GPIO0,GPIO_OUTPUT);
 	while(1){
 		os_task_delay(500);
+		//convert_float_to_str(print_string,3.1415,4);
+		//printf("PI es:%s\r\n",print_string);
+		//convert_float_to_str(print_string,-3.1415,4);
+		//printf("-1*PI es:%s\r\n",print_string);
 		gpioToggle(LED1);
 		gpioToggle(GPIO0);
 	}
