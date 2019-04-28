3ec4137b0149431d483cba04a8cffd1549029500
Gonzalo Lavigna <gonzalolavigna@gmail.com>
2019-04-28 02:14:00 -0300

    Se deja direccionado el mensaje para poder hacer una compilacion de lo que tenemos propuesto para el final

--- project.mk
+++ project.mk
@@ -22,6 +22,8 @@
 # Project name
 #PROJECT_NAME = iso-mse-2019-ejer2
 #PROJECT_NAME = iso-mse2019
-PROJECT_NAME  = iso-mse-2019-final
+#PROJECT_NAME  = iso-mse-2019-final
+#PROJECT_NAME  = iso-mse-2019-final-irq
+PROJECT_NAME  = iso-mse-2019-irq-colas
 #PROJECT_PATH = examples_c
 #PROJECT_NAME = newlib_printf_scanf
